4fb20f21c845d38f8c0f796e6fa6aef69538ea20
nicoles <nicoletbn+github@gmail.com>
2011-11-25 22:25:01 -0800

    Initial commit of modding govtracker
    it's phone gap! it's sencha! it's sencha in phone gap!

---
+++ iphone/Reptracker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Reptracker
+//
+//  Created by Nicole Aptekar on 11/11/25.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
